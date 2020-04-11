11d9b7c0a162b9c20506defc7f07e0f01f67757a
matias <matiasgonzaloacosta@gmail.com>
2020-04-11 00:28:23 -0300

    Updates

---
+++ precache-manifest.481a43d1c0dfded9c165081d91ac63cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5179ebb090fc3b9466a852204da6cbd",
+    "url": "/budget-control/index.html"
+  },
+  {
+    "revision": "ed0272c2ad9e00848e0f",
+    "url": "/budget-control/static/css/main.6464372a.chunk.css"
+  },
+  {
+    "revision": "0d3d27e508d55583dbc0",
+    "url": "/budget-control/static/js/2.6d29b249.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/budget-control/static/js/2.6d29b249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0272c2ad9e00848e0f",
+    "url": "/budget-control/static/js/main.3b1b03e9.chunk.js"
+  },
+  {
+    "revision": "f555d049a46f0bb3a528",
+    "url": "/budget-control/static/js/runtime-main.54cf824c.js"
+  }
+]);
